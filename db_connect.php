deaef3fc72f1e57eba5a9bcbd0edc37be859f8f3
Supernova-Money12 <jfollowgod25@gmail.com>
2025-08-18 14:44:34 +0100

    Create db_connect.php

---
+++ db_connect.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$username = "";
+$password = ""; // Use the correct MySQL root password
+$dbname = "native_doctors";
+$port = 3307; // Your custom MySQL port
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $dbname, $port);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
